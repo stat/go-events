889cef00e0386a24345e06b700cc3cd63a65dda1
Chris Gerber <csgerber@gmail.com>
2024-04-03 23:49:26 -0700

    adding get aircraft and its current location

--- pkg/transport/http/handlers/aircrafts/get.go
+++ pkg/transport/http/handlers/aircrafts/get.go
@@ -3,11 +3,32 @@ package aircrafts
 import (
 	"net/http"
 
+	"grid/pkg/repos/cache"
 	"grid/pkg/transport/http/respond"
 
 	"github.com/gin-gonic/gin"
 )
 
+type URIParams struct {
+	AircraftID string `uri:"aircraft_id"`
+}
+
 func Get(c *gin.Context) {
-	respond.With(c, http.StatusOK, "")
+	uriParams := &URIParams{}
+
+	if err := c.ShouldBindUri(uriParams); err != nil {
+		respond.WithError(c, http.StatusBadRequest, err)
+		return
+	}
+
+	// lookup
+
+	location, err := cache.Backend.GetAircraftLocation(uriParams.AircraftID)
+
+	if err != nil {
+		respond.WithError(c, http.StatusNotFound, err)
+		return
+	}
+
+	respond.With(c, http.StatusOK, location)
 }
