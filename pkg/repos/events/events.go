47e22d8eea2928b0d9575e91274cf6dc8478cb8e
Chris Gerber <csgerber@gmail.com>
2024-04-11 23:10:34 -0700

    implementing generics within event backend

--- pkg/repos/events/events.go
+++ pkg/repos/events/events.go
@@ -2,23 +2,146 @@ package events
 
 import (
 	"grid/pkg/env"
+	"grid/pkg/model"
+
+	// "grid/pkg/repo/backend"
+
+	// "grid/pkg/repos/events/backend"
+	"grid/pkg/repos/events/backends"
 	"grid/pkg/repos/events/provider"
 )
 
+type Repo[V model.Implementer] struct {
+	provider.Provider[V]
+}
+
+// type V model.Implementer
+
 var (
-	Backend provider.Provider
+// Backend provider.Provider[V]
+// Backend interface{}
 )
 
-func Initialize[T provider.Provider](vars *env.Vars) error {
-	var provider T
+// var (
+// Backend provider.Provider[model.Implementer]
+// Backend backend.Provider
+// )
 
-	concrete, err := provider.Initialize(vars)
+type Options[V model.Implementer] struct {
+	// Backend interface{}
+	// Backend backend.Type
+	Backend provider.Provider[V]
+}
 
-	if err != nil {
-		return err
+func (implementation *Repo[V]) InitializeFn(options *Options[V]) (func(vars *env.Vars) error, error) {
+	if options == nil {
+		options = &Options[V]{}
 	}
 
-	Backend = concrete
+	if options.Backend == nil {
+		options.Backend = &backends.Local[V]{}
+	}
 
-	return nil
+	backend := options.Backend
+
+	fn := func(vars *env.Vars) error {
+		return backend.Initialize(vars)
+	}
+
+	implementation.Provider = backend
+
+	return fn, nil
 }
+
+// // func InitializeFn(options *Options) func(vars *env.Vars) error {
+func InitializeFn[V model.Implementer](options *Options[V]) (func(vars *env.Vars) error, error) {
+	// func InitializeFn[V model.Implementer](implementer provider.Provider[V]) (func(vars *env.Vars) error, error) {
+	// backend, ok := options.Backend.(provider.Provider[model.Implementer])
+
+	// if !ok {
+	//   return nil, fmt.Errorf("could not create init function for events repo")
+	// }
+
+	backend := options.Backend
+
+	fn := func(vars *env.Vars) error {
+		return backend.Initialize(vars)
+	}
+
+	// Backend = backend
+
+	return fn, nil
+}
+
+// func Initialize[T backend.Provider](vars *env.Vars) error {
+//   var backend T
+
+//   err := backend.Initialize(vars)
+
+//   if err != nil {
+//     return err
+//   }
+
+//   Backend = backend
+
+//   return nil
+// }
+
+// func Initialize[T backend.Type, V model.Implementer](vars *env.Vars) error {
+// func Initialize(vars *env.Vars) error {
+
+//   provider
+
+//   concrete, err := provider.Initialize(vars)
+
+//   if err != nil {
+//     return err
+//   }
+
+//   Backend = concrete
+
+//   return nil
+// }
+
+// func Append[V model.Implementer](key string, v *V) error {
+//   return Backend.Append(key, v)
+// }
+
+// func Del[V model.Implementer](key string) error {
+//   return Backend.Del(key)
+// }
+
+// func DelAtIndex[V model.Implementer](key string, index int64) error {
+//   return Backend.DelAtIndex(key, index)
+// }
+
+// func DelHead[V model.Implementer](key string) (*V, error) {
+//   // TODO: implement me
+//   return nil, nil
+// }
+
+// func DelTail[V model.Implementer](key string) (*V, error) {
+//   // TODO: implement me
+//   return nil, nil
+// }
+
+// func Get[V model.Implementer](key string) (*V, error) {
+//   iface, err := Backend.Get(key)
+
+//   return nil, nil
+// }
+
+// func GetAtIndex[V model.Implementer](key string) (*V, error) {
+//   // TODO: implement me
+//   return nil, nil
+// }
+
+// func GetHead[V model.Implementer](key string) (*V, error) {
+//   // TODO: implement me
+//   return nil, nil
+// }
+
+// func GetTail[V model.Implementer](key string) (*V, error) {
+//   // TODO: implement me
+//   return nil, nil
+// }
