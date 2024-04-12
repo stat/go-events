133da414b9ec72683aeac190acb2c04120d4d2ed
Chris Gerber <csgerber@gmail.com>
2024-04-11 23:04:39 -0700

    preparing for cache generics with provider definitions

--- pkg/repos/cache/provider/provider.go
+++ pkg/repos/cache/provider/provider.go
@@ -2,13 +2,22 @@ package provider
 
 import (
 	"grid/pkg/env"
-	"grid/pkg/models"
+	"grid/pkg/model"
 )
 
-type Provider interface {
-	GetAircraftLocation(key string) (*models.LocationEvent, error)
-	GetAircraftsLocations() (map[string]*models.LocationEvent, error)
-	UpsertAircraftLocation(key string, v *models.LocationEvent) error
+// type Initializer = func[V any]
 
-	Initialize(vars *env.Vars) (Provider, error)
+// type Initializer[V any] interface {
+//   Initialize(vars *env.Vars) (Implementer[V], error)
+// }
+
+type Implementer[V model.Implementer] interface {
+	GetLatest(key string) (*V, error)
+	GetAircraftLocation(key string) (*V, error)
+	GetAircraftsLocations() (map[string]*V, error)
+	UpsertAircraftLocation(key string, v *V) error
+
+	Initialize(vars *env.Vars) error
 }
+
+type Type string
