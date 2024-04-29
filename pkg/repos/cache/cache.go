4490e76f9829f9c6319e08557f0a0d7050adc4ee
Chris Gerber <csgerber@gmail.com>
2024-04-29 04:18:37 -0700

    refactoring cache for stream implementation

--- pkg/repos/cache/cache.go
+++ pkg/repos/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"errors"
 	"grid/pkg/env"
 	"grid/pkg/model"
 	"grid/pkg/repos/cache/backends"
@@ -23,32 +24,98 @@ import (
 //	  Model    *V
 //	}
 
+var (
+	//
+	// Errors
+	//
+
+	OptionsEmptyError    = errors.New("Cache repository options are empty")
+	OptionsEnvEmptyError = errors.New("Cache repository env definitions are empty")
+)
+
+//
+// Options
+//
+
+type Options[V model.Implementer] struct {
+	Env     *env.Vars
+	Backend provider.Implementer[V]
+}
+
+func (definitions *Options[V]) Validate() error {
+	if definitions.Env == nil {
+		return OptionsEnvEmptyError
+	}
+
+	return nil
+}
+
+//
+// Repository
+//
+
 type Repo[V model.Implementer] struct {
 	provider.Implementer[V]
 }
 
-type Options[V model.Implementer] struct {
-	Backend provider.Implementer[V]
-}
-
 func (implementation *Repo[V]) InitializeFn(options *Options[V]) (func(vars *env.Vars) error, error) {
-	if options == nil {
-		options = &Options[V]{}
+	// if options == nil {
+	//   options = &Options[V]{}
+	// }
+
+	// if options.Backend == nil {
+	//   options.Backend = &backends.Local[V]{}
+	// }
+
+	// backend := options.Backend
+
+	fn := func(vars *env.Vars) error {
+		// return backend.Initialize(vars)
+		return Initialize(implementation, options)
 	}
 
+	// implementation.Implementer = backend
+
+	return fn, nil
+}
+
+//
+// Static Fns
+//
+
+func DefaultOptions[V model.Implementer]() (*Options[V], error) {
+	// TODO: implement me
+	return nil, nil
+}
+
+// TODO: refine generics and move into pkg/repo/repo.go
+func Initialize[V model.Implementer](repo *Repo[V], options *Options[V]) error {
+	// TODO: move this to default options merge
+	if options == nil {
+		return OptionsEmptyError
+	}
+
+	if err := options.Validate(); err != nil {
+		return err
+	}
+
+	// TODO: move this to default options merge
 	if options.Backend == nil {
 		options.Backend = &backends.Local[V]{}
 	}
 
+	// backend
+
 	backend := options.Backend
 
-	fn := func(vars *env.Vars) error {
-		return backend.Initialize(vars)
+	// TODO: implement env vars within options
+	if err := backend.Initialize(options.Env); err != nil {
+		return err
 	}
 
-	implementation.Implementer = backend
+	repo.Implementer = backend
 
-	return fn, nil
+	return nil
 }
 
 // type V = model.Implementer
