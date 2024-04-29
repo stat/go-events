6e171596a9e5c110a3ce5d0bc55d8fc045991d95
Chris Gerber <csgerber@gmail.com>
2024-04-29 04:19:01 -0700

    refactoring env vars for stream implementation

--- pkg/env/vars.go
+++ pkg/env/vars.go
@@ -12,22 +12,26 @@ type Vars struct {
 	// ENV
 	//
 
-	ASYNQConcurrency int    `mapstructure:"ASYNQ_CONCURRENCY"`
-	HTTPServerPort   string `mapstructure:"HTTP_SERVER_PORT"`
-	PostgresUser     string `mapstructure:"POSTGRES_USER"`
-	PostgresPassword string `mapstructure:"POSTGRES_PASSWORD"`
-	PostgresHost     string `mapstructure:"POSTGRES_HOST"`
-	PostgresPort     string `mapstructure:"POSTGRES_PORT"`
-	PostgresDB       string `mapstructure:"POSTGRES_DB"`
-	PostgresSchema   string `mapstructure:"POSTGRES_SCHEMA"`
-	PostgresTimezone string `mapstructure:"POSTGRES_TIMEZONE"`
-	RedisDB          int    `mapstructure:"REDIS_DB"`
-	RedisDBAsynq     int    `mapstructure:"REDIS_DB_ASYNQ"`
-	RedisDBCache     int    `mapstructure:"REDIS_DB_CACHE"`
-	RedisDBEvents    int    `mapstructure:"REDIS_DB_EVENTS"`
-	RedisHost        string `mapstructure:"REDIS_HOST"`
-	RedisPort        string `mapstructure:"REDIS_PORT"`
-	WebSocketPort    string `mapstructure:"WEB_SOCKET_PORT"`
+	ASYNQConcurrency int `mapstructure:"ASYNQ_CONCURRENCY"`
+
+	HTTPServerPort string `mapstructure:"HTTP_SERVER_PORT"`
+
+	// PostgresUser     string `mapstructure:"POSTGRES_USER"`
+	// PostgresPassword string `mapstructure:"POSTGRES_PASSWORD"`
+	// PostgresHost     string `mapstructure:"POSTGRES_HOST"`
+	// PostgresPort     string `mapstructure:"POSTGRES_PORT"`
+	// PostgresDB       string `mapstructure:"POSTGRES_DB"`
+	// PostgresSchema   string `mapstructure:"POSTGRES_SCHEMA"`
+	// PostgresTimezone string `mapstructure:"POSTGRES_TIMEZONE"`
+
+	RedisDB       int    `mapstructure:"REDIS_DB"`
+	RedisDBAsynq  int    `mapstructure:"REDIS_DB_ASYNQ"`
+	RedisDBCache  int    `mapstructure:"REDIS_DB_CACHE"`
+	RedisDBEvents int    `mapstructure:"REDIS_DB_EVENTS"`
+	RedisHost     string `mapstructure:"REDIS_HOST"`
+	RedisPort     string `mapstructure:"REDIS_PORT"`
+
+	WebSocketPort string `mapstructure:"WEB_SOCKET_PORT"`
 
 	// Flags
 
